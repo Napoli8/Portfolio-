8315c86224c9964aa80c3e06ed17c883f755b91f
Napoli8 <57283442+Napoli8@users.noreply.github.com>
2022-11-07 01:48:48 +0200

    Add files via upload

---
+++ nametso.js
@@ -0,0 +1,15 @@
+let navlist = document.getElementById("nav")
+let explore = document.getElementById("explore")
+let navigation = document.getElementById("navigation")
+
+
+
+
+function nav(){
+    document.getElementById('responsive').style.display = "block"
+    
+}
+
+function cancel(){
+    document.getElementById('responsive').style.display = "none"
+}
